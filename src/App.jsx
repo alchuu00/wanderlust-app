8394a3b5c019405059cce53d7271a5acbf45e72e
Alja Čekada <alja.cekada@gmail.com>
2023-09-22 15:17:29 -0700

    display name of clicked country

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,28 @@
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 import { WorldMap } from 'world-svg';
+import countryCodesToNames from "./countryCodesToNames.json";
 
 function App() {
+  const [selectedCountryName, setSelectedCountryName] = useState(null);
+
   const handleCountryClick = (countryId) => {
-    if (countryId === "US") {
-      console.log("United states of america")
+    if (countryCodesToNames.hasOwnProperty(countryId)) {
+      setSelectedCountryName(countryCodesToNames[countryId]);
+    } else {
+      setSelectedCountryName("Country not found");
     }
   }
 
   return (
     <>
-  <WorldMap tooltip={"on"} onCountryClick={handleCountryClick} landColor={"#ffffff"} hoverColor={"#3881ff"}/>
+    <div className='p-10'>{selectedCountryName
+          ? selectedCountryName
+          : "Click on a country to display it's name!"}</div>
+      
+      <WorldMap tooltip={"off"} onCountryClick={handleCountryClick} landColor={"#ffffff"} hoverColor={"#3881ff"} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
