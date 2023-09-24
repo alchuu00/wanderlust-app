d00dfe035150540a65e73198f18d23c9d6d6633a
Alja Čekada <alja.cekada@gmail.com>
2023-09-24 12:59:10 -0700

    display visited countries on a list

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,37 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import WorldMap from "react-svg-worldmap";
 import countryCodes from "./countryCodes.json";
 
 function App() {
   const [selectedCountry, setSelectedCountry] = useState(null);
+  const [visitedCountries, setVisitedCountries] = useState([]);
 
   const data = countryCodes;
 
-  const handleCountryClick = ({ countryName, countryCode, countryValue }) => {
+  const handleCountryClick = ({ countryName }) => {
     setSelectedCountry(countryName);
-    console.log(selectedCountry);
+
+    // Check if the country is already in the visited list
+    const countryIndex = visitedCountries.indexOf(countryName);
+
+    if (countryIndex !== -1) {
+      // Country is already visited, remove it from the list
+      const updatedVisitedCountries = [...visitedCountries];
+      updatedVisitedCountries.splice(countryIndex, 1);
+      setVisitedCountries(updatedVisitedCountries);
+    } else {
+      // Country is not visited, add it to the list
+      setVisitedCountries(prevVisitedCountries => [...prevVisitedCountries, countryName]);
+    }
   };
+
+  // console log the selected country
+  useEffect(() => {
+    if (selectedCountry) {
+      console.log(selectedCountry);
+    }
+  }, [selectedCountry]);
 
   return (
     <div className="App">
@@ -24,6 +44,14 @@ function App() {
           backgroundColor=""
           onClickFunction={handleCountryClick}
         />
+      </div>
+      <div>
+        <p>Visited countries:</p>
+        <ul>
+          {visitedCountries.map((country, index) => (
+            <li key={index}>{country}</li>
+          ))}
+        </ul>
       </div>
     </div>
   );
